5ec49df22f0ebf9ffe667a4f1568d158c8f0f19f
Misha <abachyanm@gmail.com>
2021-02-09 23:32:31 +0400

    Refactoring

--- js/app-dist.js
+++ js/app-dist.js
@@ -12,3 +12,48 @@ offerItemBtn.forEach((btn)=>{
         offerItemBtnLink.parentNode.parentNode.firstElementChild.classList.remove('hovered');
     })
 })
+
+/*-- Owl carousel --*/
+
+$(".members-section-carousel").owlCarousel({
+    items: 4,
+    margin: 15,
+    navText: ['<span class="arrow_triangle-left"></span>', '<span class="arrow_triangle-right"></span>'],
+    responsive : {
+        0 : {
+            items: 1,
+            nav:false
+        },
+        768 : {
+            items: 2,
+            nav: false,
+        },
+        991 : {
+            items: 3,
+            nav: true,
+        },
+        1200 : {
+            items: 4,
+            nav: true,
+        }
+    }
+});
+
+/*---  Scroll Top Button ---*/
+
+$('.footer-main__map-scroll-top').on('click', ()=>{
+    $("html, body").animate({ scrollTop: 0 }, 600);
+})
+
+/*---  Search form animation ---*/
+
+$('.top-header__search form button').on('click', ()=>{
+    $('.top-header__search form').find('input').toggleClass('search-open')
+})
+
+/*--- Mobile Navigation ---*/
+
+$('.burger-btn').on('click', function(e){
+    $(this).parent('.top-header__burger-btn').toggleClass('mobile-nav__btn-show');
+    $('.mobile-navigation').toggleClass('mobile-nav-show')
+})
